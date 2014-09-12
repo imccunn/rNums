52be89e0593c238704da21eb0e2f73517ecb1eb9
Ian McCunn <ianmccunn@gmail.com>
2014-09-12 09:59:20 -0700

    rFloat
    starting point for a random float

--- rNums.js
+++ rNums.js
@@ -1,9 +1,14 @@
 (function() {
   'use strict';
   
-  // Basic implementation of obtaining a random integer
+  // Basic implementation of obtaining a random integer within a range
   function rInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  // basic implementation of obtaining a float within a range
+  function rFloat(min, max) {
+    return Math.random() * (max - min) + min;
   }
   
 })();
