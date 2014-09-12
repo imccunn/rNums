f89630d2ae1e11aa972b29510394efb049087953
Ian McCunn <ianmccunn@gmail.com>
2014-09-12 09:56:03 -0700

    rInt
    starting point for a random integer function.

--- rNums.js
+++ rNums.js
@@ -1,5 +1,9 @@
 (function() {
   'use strict';
   
+  // Basic implementation of obtaining a random integer
+  function rInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
   
 })();
