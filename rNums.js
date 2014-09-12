409b556cc559c28c2cbf7924e651e9d9c920269c
Ian McCunn <ianmccunn@gmail.com>
2014-09-12 10:21:18 -0700

    Update rNums.js
    - added format for function descriptions/comments
    - added precision parameter to be used with random float function

--- rNums.js
+++ rNums.js
@@ -1,14 +1,30 @@
 (function() {
   'use strict';
   
+  // ****************************************
   // Basic implementation of obtaining a random integer within a range
-  function rInt(min, max) {
+  //
+  //
+  // @params min, max
+  // @return 
+  
+  function rInt( min, max ) {
+    
     return Math.floor(Math.random() * (max - min) + min);
+    
   }
   
+  // ****************************************
   // basic implementation of obtaining a float within a range
-  function rFloat(min, max) {
-    return Math.random() * (max - min) + min;
+  //
+  //
+  // @params min, max, prec
+  // @return 
+  
+  function rFloat( min, max, precision ) {
+    
+    return (Math.random() * (max - min) + min).toFixed(precision);
+    
   }
   
 })();
